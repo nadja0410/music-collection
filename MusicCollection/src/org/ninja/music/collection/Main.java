548f6438b21b41c9ead6a648b9b0dbf4683f96d8
ganna10 <janecoates10@gmail.com>
2015-07-27 23:12:43 +0200

    Test on debian

--- MusicCollection/src/org/ninja/music/collection/Main.java
+++ MusicCollection/src/org/ninja/music/collection/Main.java
@@ -7,7 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		//FolderList fList = new FolderList("M:\\Collection");
 		//FolderList fList = new FolderList("M:\\_Road Trip Music");
-		FolderList fList = new FolderList("D:\\Privat\\Music");
+		//FolderList fList = new FolderList("D:\\Privat\\Music");
+		FolderList fList = new FolderList("/local/home/coates/Documents");
 		
 		for (String folderName : fList.readFolders()) 
 		{
